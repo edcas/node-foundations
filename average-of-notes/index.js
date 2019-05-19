d9f4655ba1e7d9475bb3de36cb09db7680a5ac03
Eduard Castro <eduard.cas07@gmail.com>
2019-05-19 15:00:49 -0500

    Add average of notes

---
+++ average-of-notes/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+function averageNotes(first, second, third) {
+    return (first + second + third) / 3
+}
+
+console.log(averageNotes(2, 3, 4))
---
+++ average-of-notes/index.v2.js
@@ -0,0 +1,5 @@
+'use strict'
+
+let averageNotes = (first, second, third) => (first + second + third) / 3;
+
+console.log(averageNotes(2, 3, 4))
